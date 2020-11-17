ee29f41eb45390b49ddf62e0f8548eb614d29673
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-11-17 17:54:14 +0100

    [WIP] add SOP url annotation to prometheus rules: system rules

--- pkg/3scale/amp/component/system_monitoring.go
+++ pkg/3scale/amp/component/system_monitoring.go
@@ -113,7 +113,6 @@ func SystemAppPrometheusRules(ns string) *monitoringv1.PrometheusRule {
 						{
 							Alert: "ThreescaleSystemApp5XXRequestsHigh",
 							Annotations: map[string]string{
-								"sop_url":     ThreescaleApicastLatencyHighURL,
 								"summary":     "Job {{ $labels.job }} on {{ $labels.namespace }} has more than 50 HTTP 5xx requests in the last minute",
 								"description": "Job {{ $labels.job }} on {{ $labels.namespace }} has more than 50 HTTP 5xx requests in the last minute",
 							},
@@ -126,6 +125,7 @@ func SystemAppPrometheusRules(ns string) *monitoringv1.PrometheusRule {
 						{
 							Alert: "ThreescaleSystemAppJobDown",
 							Annotations: map[string]string{
+								"sop_url":     ThreescalePrometheusJobDownURL,
 								"summary":     "Job {{ $labels.job }} on {{ $labels.namespace }} is DOWN",
 								"description": "Job {{ $labels.job }} on {{ $labels.namespace }} is DOWN",
 							},
@@ -159,6 +159,7 @@ func SystemSidekiqPrometheusRules(ns string) *monitoringv1.PrometheusRule {
 						{
 							Alert: "ThreescaleSystemSidekiqJobDown",
 							Annotations: map[string]string{
+								"sop_url":     ThreescalePrometheusJobDownURL,
 								"summary":     "Job {{ $labels.job }} on {{ $labels.namespace }} is DOWN",
 								"description": "Job {{ $labels.job }} on {{ $labels.namespace }} is DOWN",
 							},
