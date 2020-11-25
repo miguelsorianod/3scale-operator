355cfb315f0b391829e255adb1a7e88ca5c0e1d7
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-11-25 18:50:55 +0100

    small fixes

--- pkg/3scale/amp/component/system.go
+++ pkg/3scale/amp/component/system.go
@@ -219,7 +219,7 @@ func (system *System) buildSystemBaseEnv() []v1.EnvVar {
 
 	result = append(result, system.SystemRedisEnvVars()...)
 	result = append(result, system.BackendRedisEnvVars()...)
-	bckListenerApicastRouteEnv := helper.EnvVarFromSecret("APICAST_BACKEND_ROOT_ENDPOINT", "backend-listener", "route_endpoint")
+	bckListenerApicastRouteEnv := helper.EnvVarFromSecret("APICAST_BACKEND_ROOT_ENDPOINT", BackendSecretBackendListenerSecretName, BackendSecretBackendListenerRouteEndpointFieldName)
 	bckListenerRouteEnv := helper.EnvVarFromValue("BACKEND_ROUTE", system.Options.BackendRouteEndpoint)
 	result = append(result, bckListenerApicastRouteEnv, bckListenerRouteEnv)
 
